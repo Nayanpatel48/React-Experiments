c21656d6b20373a84456b6d2ccc51a3dbeee30dc
nayan <www.xil187@gmail.com>
2024-10-31 11:41:15 +0530

    counter project Uisng REact done

---
+++ 03basicViteReact/src/App.jsx
@@ -0,0 +1,57 @@
+import {useState} from 'react';
+import './App.css'
+
+function App() {
+  //useState hook
+  //we get two things from useState() in the array form
+  //so baically setCounter is a function which will control the setCounter function
+  //We have did the syncing of state and UI
+  let [counter, setCounter]=useState(0)
+
+
+  const increaseValue = () =>{
+    counter = counter + 1 
+    setCounter(counter)
+    console.log("Value added", Math.random())
+    console.log(counter)
+  }
+
+  const removeValue = () => {
+    counter = 0
+    setCounter(counter)
+    console.log("Value added", Math.random())
+    console.log(counter)
+  }
+
+  const decreseValue = () => {
+    counter = counter - 1 
+    setCounter(counter)
+    console.log("Value added", Math.random())
+    console.log(counter)
+  }
+
+  return (
+    <>
+      <h1>Winter is Here</h1>
+      <h2>Counter value : {counter}</h2>
+
+      <button 
+      onClick={increaseValue}
+      >Increase Value By 1:{counter}</button>
+      
+      <br/>
+
+      <button
+      onClick={removeValue}
+      >Remove Value:{counter}</button>
+
+      <br/>
+
+      <button
+      onClick={decreseValue}
+      >Decrease Value By 1:{counter}</button>
+    </>
+  )
+}
+
+export default App
